90774349897a006248be9385c5132e43f4f16bf8
Vittorio Perotto <vittorio.perotto@outlook.com>
2020-11-18 15:56:42 -0300

    Template

---
+++ codes/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Selenium
+{
+    [Test Class]
+    public class UnitTest1
+    {
+        [Test Method]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
+
+// Template
